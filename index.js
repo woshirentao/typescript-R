e77e43df86464dc2af992eaeef37f1b5fcb4cc4a
rentao <jyhcrtt@163.com>
2020-01-06 22:05:07 +0800

    init

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // 类
+// 定义类
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var aPerson = new Person();
+aPerson.firstName = '任';
+aPerson.lastName = '涛';
+// 不能访问不存在的 成员变量
+// aPerson.name = '123'
+// 类型限制
+// aPerson.firstName = 123
--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
 // 类
+// 定义类
+class Person {
+  // 声明成员变量的类型
+  firstName: string
+  lastName: string
+}
+
+let aPerson = new Person()
+
+aPerson.firstName = '任'
+aPerson.lastName = '涛'
+// 不能访问不存在的 成员变量
+// aPerson.name = '123'
+// 类型限制
+// aPerson.firstName = 123
