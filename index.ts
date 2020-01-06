458af01229b1132d7a53998f300c9928bc85e8a5
rentao <jyhcrtt@163.com>
2020-01-06 21:05:28 +0800

    union 类型

--- index.ts
+++ index.ts
@@ -34,7 +34,36 @@ a = 10
   let list: any[] = [1, true, "free"];
   list[1] = 100;
 }
+// void
+/**
+ * void类型像是与any类型相反，它表示没有任何类型
+ * 当一个函数没有返回值时，你通常会见到其返回值类型是 void
+ * 只能为它赋予undefined和null
+ */
+{
+  function warnUser(): void {
+    console.log("This is my warning message");
+  }
+  let emptyValue: void = null
 
+}
+// null和undefined
+/**
+ * undefined和null两者各自有自己的类型分别叫做undefined和null,但是只能为它赋予undefined和null
+ * 默认情况下null和undefined是所有类型的子类型。除非加上--strictNullChecks,如：tsc --strictNullChecks index.ts
+ * 尽可能地使用--strictNullChecks
+ */
+{
+  // let a: undefined = 123
+  // let b: null = 34
+  let u: undefined = undefined;
+  let n: null = null;
+  // 当--strictNullChecks和联合类型一起使用时，null可以传递
+  function strictNullChecks(value: string|null|undefined): void {
+    console.log("联合类型", value);
+  }
+  strictNullChecks(null)
+}
 
 
 // 重复声明变量时，必须是同类型
