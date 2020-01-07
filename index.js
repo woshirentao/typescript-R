b28cf338c7125349f58b8e583aa6246498260588
rentao <jyhcrtt@163.com>
2020-01-07 17:16:25 +0800

    提交

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 /**
- * 接口：
- */ 
+ * 接口：类型检查器
+ * 对值所具有的结构进行类型检查,对得上才行
+ * 规定格式规范
+ */
+function checkPerson(o) {
+    // console.log(o.name);
+    o.print(o.name);
+}
+checkPerson({
+    age: 13,
+    name: '任涛',
+    print: function (name) {
+        console.log('对象定义方法');
+    }
+});
+var Persons = /** @class */ (function () {
+    function Persons() {
+        this.name = '默认名字';
+    }
+    Persons.prototype.print = function (name) {
+        console.log('类定义方法');
+    };
+    return Persons;
+}());
+checkPerson(new Persons());
+//  let a:Person = {
+//    name: ''
+//  }
--- index.ts
+++ index.ts
@@ -1,3 +1,37 @@
 /**
- * 接口：
+ * 接口：类型检查器
+ * 对值所具有的结构进行类型检查,对得上才行
+ * 规定格式规范
  */
+
+ interface Person {
+   name: string,
+   age?: number // 可选类型
+   // 定义方法
+   print(name: string): void
+ }
+
+ function checkPerson(o: Person) {
+  // console.log(o.name);
+  o.print(o.name)
+ }
+
+ checkPerson({
+   age: 13,
+   name: '任涛',
+   print(name) {
+    console.log('对象定义方法')
+   }
+ })
+
+ class Persons {
+   name: string = '默认名字'
+   print(name) {
+     console.log('类定义方法')
+   }
+ }
+ checkPerson(new Persons())
+
+//  let a:Person = {
+//    name: ''
+//  }
