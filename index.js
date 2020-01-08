1589165c52e1b4609f20d888c49446b719b8aff2
rentao <jyhcrtt@163.com>
2020-01-08 21:02:20 +0800

    init

--- index.js
+++ index.js
@@ -57,3 +57,53 @@ var add7 = function (value) {
     return value;
 };
 console.log('add7', add7('我是任涛'));
+/**
+ * 函数类型的定义方式:
+ * 1、any
+ * 2、Function
+ * 4、类型别名
+ * 5、函数类型接口
+ */
+// 3、
+var func = function (name) {
+    return name == 'rentao';
+};
+/**
+ * this指向的问题:
+ * this的值在函数被调用的时候才会指定,在返回一个函数或将函数当做参数传递的时候，很难确定this的值
+ */
+var obj = {
+    w: 10,
+    h: 10,
+    getArea: function () {
+        var _this = this;
+        // return function() {
+        //   let area = this.w * this.h // 此时this指向window
+        //   console.log(area)
+        // }
+        //解决办法一：返回箭头函数,箭头函数能保存函数创建时的 this值，而不是调用时的值,默认情况下，编译器不会指出this的指向问题，给编译器设置了--noImplicitThis标记，会指出this的问题
+        return function () {
+            var area = _this.w * _this.h; // 此时this指向window
+            console.log(area);
+        };
+    }
+};
+var areaFunc = obj.getArea();
+var area = areaFunc(); // 由window调用
+/**
+ * 函数重载：
+ * 允许用相同的名字创建参数不同的函数
+ */
+var suits = ["hearts", "spades", "clubs", "diamonds"];
+function pickCard(x) {
+    if (typeof x == "object") {
+        var pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    else if (typeof x == "number") {
+        var pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+var pickedCard2 = pickCard(15);
+console.log(pickedCard2);
