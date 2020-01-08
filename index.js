5c3ef3c1b5e569e22f5275329c5be0fe6ff6bcb0
rentao <jyhcrtt@163.com>
2020-01-08 15:48:38 +0800

    init

--- index.js
+++ index.js
@@ -6,6 +6,19 @@
  * 作用：1、作为类型检测
  * 2、类可以实现接口
  */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function checkPerson(o) {
     o.print(o.name);
 }
@@ -43,8 +56,17 @@ var Rentao = /** @class */ (function () {
     };
     return Rentao;
 }());
+var Rentao1 = /** @class */ (function () {
+    function Rentao1() {
+        this.name = '类实现了接口';
+    }
+    Rentao1.prototype.print = function (name) {
+        console.log(this.name);
+    };
+    return Rentao1;
+}());
 // let rt: Rentao = new Rentao()
-// 也可以声明为接口类型
+// 也可以将变量声明为接口类型
 var rt = new Rentao();
 rt.print('');
 // 对于函数类型的类型检查来说，函数的参数名不需要与接口里定义的名字相匹配，但是类型必须要对得上
@@ -54,3 +76,62 @@ var p2 = function (soc, subStr) {
 };
 // 当函数一样调用
 p2('hhhh', '123');
+// 断言与接口的使用
+var person = {};
+// let person = <Person>{
+//   name: '第二种用法'
+// }
+person.name = '断言person'; // 会报错，找不到name属性，需要使用断言告诉编译器
+console.log(person.name);
+var shape = {
+    width: 12,
+    height: 12,
+    color: 'red'
+};
+// 类实现接口
+var ShapeClass = /** @class */ (function () {
+    function ShapeClass() {
+    }
+    return ShapeClass;
+}());
+/**
+ * 接口继承类
+ * 当接口继承了一个类时，它会继承类的成员但不包括其实现。
+ */
+var Components = /** @class */ (function () {
+    function Components() {
+    }
+    Components.prototype.display = function () {
+    };
+    return Components;
+}());
+var button = {
+    width: 0,
+    height: 0,
+    display: function () {
+        console.log('displaying');
+    }
+};
+button.display();
+var dic = {
+    name: 'RT'
+};
+console.log(dic.name);
+var array = ['1', '2', '3'];
+console.log(array);
+/**
+ * 注意：可以同时使用两种类型的索引，但是数字索引的返回值必须是字符串索引返回值类型的子类型
+ * 这是因为当使用 number来索引时，JavaScript会将它转换成string然后再去索引对象
+ */
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Dog;
+}(Animal));
--- index.ts
+++ index.ts
@@ -49,15 +49,23 @@ class Rentao implements Person {
     console.log(this.name)
   }
 }
+// 可以实现多个接口
+interface Person3 {
+  height: number
+}
+class Rentao1 implements Person, Person3 {
+  name: string = '类实现了接口'
+  height: number
+  print(name: string): void {
+    console.log(this.name)
+  }
+}
+
 // let rt: Rentao = new Rentao()
-// 也可以声明为接口类型
+// 也可以将变量声明为接口类型
 let rt: Person = new Rentao()
 rt.print('')
 
-// 额外的属性检查
-interface Person1 {
-  [propName: string]: any // 最佳的方式是添加一个字符串索引签名
-}
 /**
  * 函数类型
  * 可以理解为声明了一个匿名函数类型
@@ -72,3 +80,119 @@ let p2: Person2 = function(soc: string, subStr: string): boolean {
 }
 // 当函数一样调用
 p2('hhhh', '123')
+
+// 断言与接口的使用
+let person = {} as Person
+// let person = <Person>{
+//   name: '第二种用法'
+// }
+person.name = '断言person' // 会报错，找不到name属性，需要使用断言告诉编译器
+console.log(person.name)
+
+
+
+/**
+ * 接口继承: 从一个接口里复制成员到另一个接口里
+ * 可以实现多继承
+ */
+interface Shape {
+  width: number
+}
+interface Shape1 {
+  color: string
+}
+// 接口多继承
+interface Square extends Shape, Shape1 {
+  height: number
+}
+let shape: Square = {
+  width: 12,
+  height: 12,
+  color: 'red'
+}
+// 类实现接口
+class ShapeClass implements Square {
+  width: number
+  height: number
+  color: string
+}
+/**
+ * 接口继承类
+ * 当接口继承了一个类时，它会继承类的成员但不包括其实现。
+ */ 
+class Components {
+  width: number
+  height: number
+  display(): void {
+
+  }
+}
+// 继承了Components的width和height，还有display方法
+interface Button extends Components {
+
+}
+
+let button: Button = {
+  width: 0,
+  height: 0,
+  display(): void {
+    console.log('displaying')
+  }
+}
+button.display()
+
+/**
+ * 可索引的类型:
+ * TypeScript支持两种索引签名：字符串和数字
+ * 数字索引可以定义数组，但是并没有数组特有的方法和属性
+ * 字符串索引可以定义对象
+ */ 
+interface StringDictionary {
+  [propName: string]: string
+}
+let dic: StringDictionary = {
+  name: 'RT'
+}
+console.log(dic.name)
+
+interface NumberArray {
+  [index: number]: string
+}
+let array: NumberArray = ['1','2','3']
+console.log(array)
+
+// 使用：额外的属性检查
+interface Person1 {
+  [propName: string]: any // 最佳的方式是添加一个字符串索引签名
+}
+
+/**
+ * 注意：可以同时使用两种类型的索引，但是数字索引的返回值必须是字符串索引返回值类型的子类型
+ * 这是因为当使用 number来索引时，JavaScript会将它转换成string然后再去索引对象
+ */
+class Animal {
+  name: string;
+}
+class Dog extends Animal {
+  breed: string;
+}
+interface NotOkay {
+  [x: number]: Dog;
+  [x: string]: Animal;
+}
+/**
+ * 注意：字符串索引签名，必须确保所有属性与其返回值类型相匹配
+ * 因为字符串索引声明了 obj.property和obj["property"]两种形式都可以
+ * 数字索引签名没有这样的要求
+ */
+// 字符串索引
+interface NumberDictionary {
+  [index: string]: number;
+  length: number;    // 可以，length是number类型
+  // name: boolean       // 错误，`name`的类型与索引类型返回值的类型不匹配，必须都保持同一个类型
+}
+// 数字索引
+interface inter {
+  [index: number]: number
+  length: string
+}
